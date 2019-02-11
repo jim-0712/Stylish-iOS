c22a4b86da9a4af27b4f0eb9f6e126d70c7fc3fc
Luke <wuchiwei17@gmail.com>
2019-02-11 13:20:34 +0800

    Init commit

---
+++ STYLiSH/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  STYLiSH
+//
+//  Created by WU CHIH WEI on 2019/2/11.
+//  Copyright © 2019 WU CHIH WEI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
