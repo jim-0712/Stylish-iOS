4aaf6cd26cde0c121d34f0358463062ed5a2fb40
Savannah Su <tingchen0626@gmail.com>
2020-01-06 15:32:03 +0800

    fixed the layout

--- STYLiSH/Jim/ChatJimViewController.swift
+++ STYLiSH/Jim/ChatJimViewController.swift
@@ -27,19 +27,23 @@ class ChatJimViewController: UIViewController {
   
   var questionType = ""
   @IBAction func questionSegAction(_ sender: UISegmentedControl) {
-    switch sender.selectedSegmentIndex{
+    
+    switch sender.selectedSegmentIndex {
     case 0 :
       questionType = "0"
+      self.answerTextView.text = ""
     case 1 :
       questionType = "1"
+      self.answerTextView.text = ""
     case 2 :
       questionType = "2"
+        self.answerTextView.text = "STYLish以會員制度進行線上交易，為您提供簡單快速且專業的購物流程，首次購物請先加入會員再進行購物。購物流程：將您喜歡的商品加入購物車→ 登入會員資料→選擇付款方式→LOVFEE出貨通知→收到商品。"
     default:
       questionType = "3"
+        self.answerTextView.text = "7-11超商取貨付款（包裹材積限制：長45cm*寬30cm*高30cm）、中華郵政（若付款付款金額為零，故務必於訂單內填寫與身分證上相符的姓名，領取包裹時提供身分證以利核對）。"
+        
     }
   }
-  
-  
   
 }
   
