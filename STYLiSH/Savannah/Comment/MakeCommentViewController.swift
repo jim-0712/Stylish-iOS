b5c09f3cf2d496ebf9201f102eb71c3d07456f1a
Savannah Su <tingchen0626@gmail.com>
2020-01-08 13:19:34 +0800

    finished the star button

--- STYLiSH/Savannah/Comment/MakeCommentViewController.swift
+++ STYLiSH/Savannah/Comment/MakeCommentViewController.swift
@@ -12,7 +12,58 @@ class MakeCommentViewController: UIViewController {
 
     @IBOutlet weak var userLabel: UILabel!
     
+    @IBOutlet weak var oneStarBtn: UIButton!
+    @IBOutlet weak var twoStarsBtn: UIButton!
+    @IBOutlet weak var threeStarsBtn: UIButton!
+    @IBOutlet weak var fourStarsBtn: UIButton!
+    @IBOutlet weak var fiveStarsBtn: UIButton!
+    
+    var star = ""
+    
+    @IBAction func oneStarBtn(_ sender: Any) {
+        oneStarBtn.isSelected = true
+        
+        star = "1"
+    }
+    @IBAction func twoStarsBtn(_ sender: Any) {
+        oneStarBtn.isSelected = true
+        twoStarsBtn.isSelected = true
+        
+        star = "2"
+    }
+    @IBAction func threeStarsBtn(_ sender: Any) {
+        oneStarBtn.isSelected = true
+        twoStarsBtn.isSelected = true
+        threeStarsBtn.isSelected = true
+        
+        star = "3"
+    }
+    @IBAction func fourStarsBtn(_ sender: Any) {
+        oneStarBtn.isSelected = true
+        twoStarsBtn.isSelected = true
+        threeStarsBtn.isSelected = true
+        fourStarsBtn.isSelected = true
+        
+        star = "4"
+    }
+    @IBAction func fiveStarsBtn(_ sender: Any) {
+        oneStarBtn.isSelected = true
+        twoStarsBtn.isSelected = true
+        threeStarsBtn.isSelected = true
+        fourStarsBtn.isSelected = true
+        fiveStarsBtn.isSelected = true
+        
+        star = "5"
+    }
+    
+    @IBOutlet weak var commentTF: UITextField!
+    @IBAction func postButton(_ sender: Any) {
+        
+    }
+    
+    
     override func viewDidLoad() {
+        
         guard let userEmail = UserDefaults.standard.value(forKey: "email") as? String else {return}
         userLabel.text = "Hi, \(userEmail)"
         super.viewDidLoad()
